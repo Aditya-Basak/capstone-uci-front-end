fa81e38eca11c0037d8bd911df0ccac7eca4c177
Aditya Basak <aditya.basak.11@gmail.com>
2021-04-27 14:22:42 -0500

    SportsCon icon redirects to login page

--- src/components/RegisterHeader.js
+++ src/components/RegisterHeader.js
@@ -5,7 +5,7 @@ class RegisterHeader extends React.Component{
     render() {
         return(
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="h3" href="#">
+                <a class="h3" href="/">
                     SportsCon
                 </a>
             </nav>
