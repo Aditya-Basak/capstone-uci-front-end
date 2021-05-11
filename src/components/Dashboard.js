c55e2800dc582c4fbddb349b1ab5fcfe3445dc7f
sindhusingamneni <sindhusingamneni@gmail.com>
2021-05-10 22:44:37 -0700

    event details and join event

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,18 +1,119 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from './RegisterHeader'
 import {Link} from 'react-router-dom';
+import axios from 'axios'
 
 function Dashboard(props){
+    const[hostedEvents, setHostedEvents] = useState([])
+    const[joinedEvents, setJoinedEvents] = useState([])
+    const[eventsHosted, setEventsHosted] = useState([])
+    const[eventsjoined, setEventsJoined] = useState([])
 
+    useEffect(() => {
+        const fetchData = async () => {
+
+        await axios.get('http://localhost:8080/api/get_user_events', {
+            params:{
+                user_id: 24
+            }
+        })
+        .then((response) => {
+            setHostedEvents(response.data.hosted);
+            setJoinedEvents(response.data.joined);
+        })
+        .catch((err) => {
+            console.log(err);
+        }) 
+        console.log(hostedEvents)
+        console.log(joinedEvents)
+        
+
+        const tempHostedEvents= [];
+        const hostedEventsResults = hostedEvents.map((item) => {
+            return axios.get('http://localhost:8080/api/get_event', {
+                params:{
+                    user_id: 24,
+                    event_id: item
+                }
+            })
+        })
+        Promise.all(hostedEventsResults).then(data => {
+            data.map((item) =>{
+                tempHostedEvents.push(item.data)
+            })
+
+            setEventsHosted(tempHostedEvents)
+        })
+        console.log("hosted events:")
+        console.log(eventsHosted)
+
+
+        const tempJoinedEvents= [];
+        const joinedEventsResults = joinedEvents.map((item) => {
+            return axios.get('http://localhost:8080/api/get_event', {
+                params:{
+                    user_id: 24,
+                    event_id: item
+                }
+            })
+        })
+        
+        Promise.all(joinedEventsResults).then(data => {
+            data.map((item) =>{
+                tempJoinedEvents.push(item.data)
+            })
+
+            setEventsJoined(tempJoinedEvents)
+        })
+        console.log("joined events:")
+        console.log(eventsjoined)
+        
+    };
+
+        fetchData();
+
+    }, [hostedEvents, joinedEvents])
 
     return (
         <div>
             <Header/>
             <br/>
+            <div className="card">
+                <div className="card-body">
+                    {eventsHosted && eventsHosted.map((item) => (
+                        <ul className="list-group">
+                            <li class="list-group-item" key={item.id}>
+                                <Link to={{pathname: '/event',
+                                    dashboardProps: {
+                                    event_id: item.id
+                                }}}>
+                                    {item.name}
+                                </Link>
+                            </li>
+                        </ul>
+                        ))}
+
+
+                    {eventsjoined && eventsjoined.map((item) => (
+                        <ul className="list-group">
+                            <li class="list-group-item" key={item.id}>
+                                <Link to={{pathname: '/event',
+                                    dashboardProps: {
+                                    event_id: item.id
+                                }}}>
+                                    {item.name}
+                                </Link>
+                            </li>
+                        </ul>
+                        ))}
+
+                    <div/>
+                </div>
+         </div>
             <Link to="/" style={{ textDecoration: "none" }}>
                 <button color="link" className="registerButton"> Logout </button>
             </Link>
-         </div>
+        </div>
 
     )
 }
---
+++ src/components/Event.js
@@ -0,0 +1,129 @@
+import React, {useState, useEffect} from 'react'
+import Header from './RegisterHeader'
+import axios from 'axios'
+
+
+function Event(props){
+    const[state, setState] = useState({
+        event_id: props.location.dashboardProps.event_id,
+        user_id: 16
+    })
+
+    const[joinedMessage, setJoinedMessage] = useState('');
+
+    const[eventState, setEventState] = useState({
+        name: "",
+        description: "",
+        location: "",
+        date: "",
+        event_type: "",
+        limit: 0,
+        scope: "",
+        attendees: []
+    })
+    const[showJoin, setShowJoin] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await axios.get('http://localhost:8080/api/get_event', {
+            params:{
+                user_id: state.user_id,
+                event_id: state.event_id
+            }
+        })
+        .then((response) => {
+            console.log(response)
+            const date = new Date(response.data.time*1000);
+            let dateFormat = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(date)
+            {if(response.data.host.id != state.user_id && response.data.limit>0 && !response.data.attendees.some(item => item.id === state.user_id)){
+                setShowJoin(true)
+            }}
+            setEventState({
+                name: response.data.name,
+                description: response.data.description,
+                location: response.data.location,
+                date: dateFormat,
+                event_type: response.data.event_type,
+                limit: response.data.limit,
+                scope: response.data.scope,
+                attendees: response.data.attendees
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        }) 
+        }
+
+        fetchData();
+    }, [showJoin]);
+
+    async function handleSubmit (event){
+        console.log("joining")
+        event.preventDefault();
+
+        await axios.post('http://localhost:8080/api/join_event', {
+            },
+            {
+            params: {
+                user_id: state.user_id,
+                event_id: state.event_id
+            }
+        })
+        .then((response) => {
+            if(response.status === 200){
+                setJoinedMessage("You have joined this event!");
+                setShowJoin(false)
+            }
+        })
+
+    }
+
+        return (
+            <div>
+            <Header/>
+            <br/>
+            {console.log(state.event_id)}
+            {console.log(eventState)}
+            {joinedMessage && <div className="registeredMessage"> {joinedMessage} </div>}
+            <h2 className="eventTitle" >{eventState.name}</h2>
+            <div className="card">
+                <div className="card-body">
+                    <h3 >Description</h3>
+                    {eventState.description}
+
+                </div>
+            </div>
+
+            <br/>
+            <h4 className="eventTitle">Event Details:</h4>
+            <div className="card">
+                <div className="card-body">
+                    <b>Location:</b> {eventState.location}
+                    {<br/>}
+                    <b>Date/Time:</b> {eventState.date}
+                    <br/>
+                    <b>Event Type:</b> {eventState.event_type}
+                    {<br/>}
+                    <b>Remaining spots:</b> {eventState.limit}
+                    {<br/>}
+                    {<br/>}
+                    <b>Attendees:</b> {eventState.attendees.map((item) => (
+                            <li  key={item.id}>
+                                
+                                    {item.name}
+                                
+                            </li>))}
+                            {<br/>}
+                            {<br/>}
+                    This is a {eventState.scope} event
+
+                </div>
+            </div>
+            {showJoin && <button onClick={handleSubmit} className="joinEventButton"> Join Event </button>}
+            <button  className="editEventButton"> Edit Event </button>
+            </div>
+        )
+
+}
+
+export default Event;
