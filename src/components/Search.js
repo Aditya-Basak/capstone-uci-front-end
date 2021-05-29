d3068a6ffe9c517d4f85cf18331fff1d6d82c146
Aditya Basak <aditya.basak.11@gmail.com>
2021-05-29 13:53:10 -0700

    Redesigned Search Page

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,10 @@ import React, {useState, useEffect} from 'react'
 import Header from './RegisterHeader'
 import {Link} from 'react-router-dom';
 import { useHistory } from "react-router-dom";
-import {Button, Form, Col, Container} from 'react-bootstrap';
 import axios from 'axios'
-import moment from 'moment'
+import moment from 'moment';
+import {Button, Tab, Tabs, Container, Row, Col, Form, ListGroup} from 'react-bootstrap';
+
 
 function Search(props){
 
@@ -67,18 +68,18 @@ function Search(props){
             <Header user_id= {props.location.user_id}/>
 
             <Container fluid>
-            <div class="experiment-body">
+            <div class="experiment-body-search-upper">
             <Form>
                 <Form.Row className="align-items-center">
-                    <Col className="my-1">
+                    <Col sm={3}>
                         <Form.Label className={"search_filter_label"}>Keyword(s):</Form.Label>
                         <Form.Control id="event_name"  placeholder="Keyword(s)" value={state.event_name} onChange={handleChange} />
                     </Col>
-                    <Col  className="my-1">
+                    <Col  sm={3}>
                     <Form.Label className={"search_filter_label"}>Location:</Form.Label>
                         <Form.Control id="location"  placeholder="Location" value={state.location} onChange={handleChange} />
                     </Col>
-                    <Col  className="my-1">
+                    <Col  sm={3}>
                     <Form.Label className={"search_filter_label"}>Event Type:</Form.Label>
                         <Form.Control as="select"  id="event_type" value={state.event_type} onChange={handleChange}>
                             <option hidden value>Select an event type</option>
@@ -89,37 +90,36 @@ function Search(props){
                             <option value="cricket">Cricket</option>
                         </Form.Control>
                     </Col>
-                    <Col  className="my-1">
+                    <Col  sm={3}>
                     <Form.Label className={"search_filter_label"}>Date:</Form.Label>
                     <Form.Control  input id="date"  type= "date" min={moment().format("YYYY-MM-DD")} value={state.date} onChange={handleChange} />
                     </Col>  
-                       
                 </Form.Row>
-                <Button onClick={fetchData} >Update</Button>
+                <Button variant="custom-search" onClick={fetchData} >Update</Button>
             </Form>
             </div>
             </Container>
 
-            <div className="card">
-                <div className="card-body">
+            <div className="experiment-body-search-lower">
+                <div >
                 {searchResults.length===0 && <div className="registeredMessage"> {noResults} </div>}
                     
                     {searchResults && searchResults.map((item) => (
-                        <ul className="list-group">
-                            <li class="list-group-item d-flex justify-content-between align-items-center" key={item.id} >
+                        <ListGroup>
+                            <li class="modified-list2 d-flex justify-content-between align-items-center" key={item.id} >
                                 <Link to={{pathname: '/event',
                                     componentProps: {
                                     event_id: item.id,
                                     user_id: props.location.user_id
-                                }}}>
+                                }}} className="custom-color">
                                     {item.name}
                                 </Link>
                             </li>
-                        </ul>
+                        </ListGroup>
                     ))}
                 </div>
             </div>
-            {<button onClick={backToDashboard} className="backButton" > Go Back </button>}
+            <Button variant="outline-warning" size="lg" onClick={backToDashboard} className="backButton" > Go Back </Button>
         </div>
 
     )
