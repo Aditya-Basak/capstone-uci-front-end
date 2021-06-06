037c9715e7c65c6f7c69d0a0ecdbbbc2a18cddd6
Sindhu Singamneni <sindhusingamneni@gmail.com>
2021-06-05 18:59:51 -0700

    Event bugs (#37)
    * should not edit attendees number below who is already attending
    
    * attendees list bugs

--- src/components/PastEvent.js
+++ src/components/PastEvent.js
@@ -226,6 +226,23 @@ function PastEvent(props){
                             {eventState.attendees && eventState.attendees.map((item) => 
                                 (
                                     <ListGroup>
+
+                                        {item.id == state.user_id && 
+                                            <li class="modified-list-attendees d-flex justify-content-between align-items-center" key={item.id} >
+
+                                                {item.image=== null &&
+                                                    <Image className={"event-image"} src={defaultImage} roundedCircle/>
+                                                }
+                                                {item.image!== null &&
+                                                    <Image className={"event-image"} src={item.image} roundedCircle/>
+                                                }
+                                                
+                                                <Link  to={{pathname: '/userProfile/' + state.user_id + "/" + item.id + "/" + true }} className="custom-color" style={{ textDecoration: "none" }}>
+                                                {item.name}
+                                                </Link>
+                                            </li>
+                                        }
+
                                         {item.id != state.user_id && 
                                             <li class="modified-list-attendees d-flex justify-content-between align-items-center" key={item.id} >
 
